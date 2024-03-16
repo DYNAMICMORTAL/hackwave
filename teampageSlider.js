586470cccde8d1896c6b39d13fd7d3da2f279d4c
Mihir <aminvasudev6@gmail.com>
2024-03-16 22:54:23 +0530

    Team Page v1.0.3

---
+++ teampageSlider.js
@@ -0,0 +1,21 @@
+// const slider = document.querySelector('.slider');
+// const prevSlideBtn = document.querySelector('.prev-slide');
+// const nextSlideBtn = document.querySelector('.next-slide');
+// const slides = document.querySelectorAll('.slide');
+
+// let currentIndex = 0;
+
+// function showSlide(index) {
+//   const slideWidth = slides[0].offsetWidth;
+//   slider.style.transform = `translateX(-${slideWidth * index}px)`;
+// }
+
+// prevSlideBtn.addEventListener('click', () => {
+//   currentIndex = currentIndex > 0 ? currentIndex - 1 : slides.length - 1;
+//   showSlide(currentIndex);
+// });
+
+// nextSlideBtn.addEventListener('click', () => {
+//   currentIndex = currentIndex < slides.length - 1 ? currentIndex + 1 : 0;
+//   showSlide(currentIndex);
+// });
